7bf26a86a9316a8e938ca989a1b94f816b4aab64
torgodly <torgodly@gmail.com>
2023-04-12 20:22:26 +0200

    fix user link

--- resources/views/livewire/page/edit.blade.php
+++ resources/views/livewire/page/edit.blade.php
@@ -734,7 +734,7 @@
 
                         </button>
 
-                        <a href="{{Auth::user()->link()}}" id="UserLink" target="_blank"
+                        <a href="https://{{Auth::user()->link()}}" id="UserLink" target="_blank"
                            class="rounded-full bg-[#f4812a] p-2 text-white shadow-sm hover:bg-[#f4812a] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#f4812a]">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                                  stroke="currentColor" class="w-5 h-5">
