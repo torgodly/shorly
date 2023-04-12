35c7d230e80171aa2a3bdff03298203fff26b923
torgodly <torgodly@gmail.com>
2023-04-12 14:16:38 +0200

    app usel

--- resources/views/messages/create.blade.php
+++ resources/views/messages/create.blade.php
@@ -116,7 +116,7 @@
                             <div class="mt-10 w-full h-fit flex justify-center px-2">
                                 <footer class="flex gap-x-1 justify-center items-center">
                                     <span class="font-footer ">made by <a
-                                            href="{{env('app_url')}}"><strong>shor.ly</strong></a></span>
+                                            href="{{env('APP_URL')}}"><strong>shor.ly</strong></a></span>
                                 </footer>
 
                             </div>
