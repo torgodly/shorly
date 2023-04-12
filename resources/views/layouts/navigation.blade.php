6333d04abbc7bf2800fe7bc5eb6e81f5359789ff
torgodly <torgodly@gmail.com>
2023-04-12 15:00:50 +0200

    add page to dropdown mobile

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -55,7 +55,7 @@
                             <x-dropdown-link :href="route('logout')"
                                              onclick="event.preventDefault();
                                                 this.closest('form').submit();">
-                                {{ __('Log Ou') }}
+                                {{ __('Log Out') }}
                             </x-dropdown-link>
                         </form>
                     </x-slot>
