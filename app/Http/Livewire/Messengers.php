2be76604127f6afd8ed2b76a008576da9f4029c5
torgodly <torgodly@gmail.com>
2023-04-15 22:35:42 +0200

    extact username from url if givien istaed of user name in Messengers.php

--- app/Http/Livewire/Messengers.php
+++ app/Http/Livewire/Messengers.php
@@ -26,6 +26,53 @@ class Messengers extends Component
         'phone' => 'string|max:255|nullable',
     ];
 
+    public function extractUsername($platform, $input)
+    {
+        // Check if input matches any of the patterns for supported platforms
+        switch ($platform) {
+            case 'messenger':
+                $pattern = '/^(?:https?:\/\/)?(?:www\.)?(?:(?:facebook\.com|fb\.me)\/(?:(?:\w\.)*#!\/)?(?:pages\/)?(?:[^\s\/]+\/)?([^\s\/?]+)(?:(?:\/)?|\?)?|(?:m\.me\/([^\s\/?]+)))/';
+                break;
+            case 'telegram':
+                $pattern = '/(?:https?:\/\/)?(?:www\.)?t\.me\/([a-zA-Z0-9_]+)(?:\/)?/';
+                break;
+            case 'skype':
+                $pattern = '/(?:https?:\/\/)?(?:www\.)?(?:(?:join\.)?skype\.com\/(?:invite\/)?|skype:)([a-zA-Z0-9_\-\.]+)/';
+                break;
+            default:
+                return $input;
+        }
+
+        // If the input is a URL and it's a Facebook URL, extract the username or user ID
+        if (preg_match($pattern, $input, $matches)) {
+            if (strpos($input, 'facebook.com') !== false || strpos($input, 'fb.me') !== false) {
+                $username = preg_match('/^(?:https?:\/\/)?(?:www\.)?(?:facebook|messenger)\.com\/(?!profile\.php).*?([^\/?]+)/', $input, $matches) ? ($matches[1] ?? '') : null;
+                $user_id = preg_match('/id=(\d+)/', $input, $matches) ? ($matches[1] ?? '') : null;
+                return $username ?? $user_id ?? null;
+            } else {
+                return isset($matches[1]) ? $matches[1] : null;
+            }
+        }
+
+        // Return the input as it is
+        return $input;
+    }
+
+    public function updatedMessenger()
+    {
+        $this->messenger = $this->extractUsername('messenger', $this->messenger);
+    }
+
+    public function updatedTelegram()
+    {
+        $this->telegram = $this->extractUsername('telegram', $this->telegram);
+    }
+
+    public function updatedSkype()
+    {
+        $this->skype = $this->extractUsername('skype', $this->skype);
+    }
+
     public function mount()
     {
         // Retrieve the user's messengers and convert them to an array
