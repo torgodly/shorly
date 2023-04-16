8d6f91bb35823b109592ad90d80112ba90e3c1b1
torgodly <torgodly@gmail.com>
2023-04-16 22:27:49 +0200

    edit dashbord

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,14 +1,50 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 ">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg max-h-screen relative">
-                <div class="p-6 text-gray-900">
+                <div class="p-6 text-gray-900 space-y-5">
+                    <div class="flex justify-between">
+                        <div class="flex">
+                            @if (file_exists('images/UserAvatar/' . Auth::user()->id . '.png'))
+                                <img src="{{ 'images/UserAvatar/' . Auth::user()->id . '.png' }}"
+                                     class="w-14 h-14 rounded-full">
+
+                            @else
+                                <img src="https://api.dicebear.com/6.x/notionists/svg?seed={{Auth::user()->username}}"
+                                     alt="Avatar" class="w-14 h-14 rounded-full">
+
+                            @endif
+
+                            <div class="flex gap-5 justify-center items-center ml-4">
+                                <p class="text-xl font-semibold text-[#f4812a]">{{Auth::user()->link()}}</p>
+                                <div class="flex gap-3 justify-center items-center">
+                                    <button>
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 01-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 011.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 00-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 01-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5a3.375 3.375 0 00-3.375-3.375H9.75" />
+                                        </svg>
+                                    </button>
+
+
+                                    <button>
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 4.875c0-.621.504-1.125 1.125-1.125h4.5c.621 0 1.125.504 1.125 1.125v4.5c0 .621-.504 1.125-1.125 1.125h-4.5A1.125 1.125 0 013.75 9.375v-4.5zM3.75 14.625c0-.621.504-1.125 1.125-1.125h4.5c.621 0 1.125.504 1.125 1.125v4.5c0 .621-.504 1.125-1.125 1.125h-4.5a1.125 1.125 0 01-1.125-1.125v-4.5zM13.5 4.875c0-.621.504-1.125 1.125-1.125h4.5c.621 0 1.125.504 1.125 1.125v4.5c0 .621-.504 1.125-1.125 1.125h-4.5A1.125 1.125 0 0113.5 9.375v-4.5z" />
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 6.75h.75v.75h-.75v-.75zM6.75 16.5h.75v.75h-.75v-.75zM16.5 6.75h.75v.75h-.75v-.75zM13.5 13.5h.75v.75h-.75v-.75zM13.5 19.5h.75v.75h-.75v-.75zM19.5 13.5h.75v.75h-.75v-.75zM19.5 19.5h.75v.75h-.75v-.75zM16.5 16.5h.75v.75h-.75v-.75z" />
+                                        </svg>
+                                    </button>
+
+                                </div>
+
+                            </div>
+                        </div>
+                        <div class="flex justify-center items-center">
+                            <a href="{{route('page.edit')}}" class="bg-[#f4812a] hover:bg-[#DE8C1B] text-white font-bold py-2 px-4 rounded">
+                                Edit
+                            </a>
+                        </div>
+                    </div>
+
                     @livewire('page.chart')
 
                 </div>
