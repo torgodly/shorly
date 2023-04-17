da2d85610034c4ac62609e6ca6aa690a9223256c
torgodly <torgodly@gmail.com>
2023-04-17 20:40:50 +0200

    extend app layoute

--- resources/views/pages/show.blade.php
+++ resources/views/pages/show.blade.php
@@ -1,31 +1,4 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+<x-app-layout>
+    <livewire:page.show :model="$user">
 
-    <title>{{ config('app.name', 'shorly') }}</title>
-
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/js/all.min.js"></script>
-    {{--        <script src="https://cdn.tailwindcss.com"></script>--}}
-
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Arabic:wght@700&display=swap" rel="stylesheet">
-
-    <!-- Scripts -->
-           @vite(['resources/css/app.css', 'resources/js/app.js'])
-</head>
-<body class="font-sans antialiased">
-<div class="min-h-screen bg-white">
-    <!-- page Content -->
-    <main>
-        <livewire:page.show :model="$user">
-    </main>
-
-</div>
-@livewireScripts
-</body>
-</html>
+</x-app-layout>
