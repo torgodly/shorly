dc1b3b3dedb5522b51c794b100dfc8c26a3359e8
torgodly <torgodly@gmail.com>
2023-04-22 05:19:38 +0200

    update store

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -32,14 +32,16 @@ class RegisteredUserController extends Controller
     {
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:'.User::class],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:' . User::class],
+            'username' => ['required', 'string', 'max:255', 'unique:' . User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
-            'username' => substr($request->email, 0, strpos($request->email, '@')),
+//            'username' => substr($request->email, 0, strpos($request->email, '@')),
+            'username' => $request->username,
             'password' => Hash::make($request->password),
         ]);
 
