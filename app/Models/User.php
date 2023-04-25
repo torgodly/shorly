e9de6071a5d49bafcba19ff1a6397832563d9248
torgodly <torgodly@gmail.com>
2023-04-25 02:12:56 +0200

    fix

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,7 +11,7 @@ use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Shetabit\Visitor\Traits\Visitable;
+use torgodly\Visitor\Traits\Visitable;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
