571b8db5ac11a4f15a54c7c70d421b16aa3aacc5
torgodly <torgodly@gmail.com>
2023-04-27 15:17:31 +0200

    fix meta for messages

--- resources/views/messages/create.blade.php
+++ resources/views/messages/create.blade.php
@@ -7,7 +7,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="csrf-token"  content="{{ csrf_token() }}">
 
     <title>{{$user->name}} </title>
     <meta property="og:title" content="صارحني برسالة سرية {{$user->name}}">
